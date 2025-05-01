d3f260c1ab1f4128dd714f22e3660342cd92eb39
anthonyRanivoarison <antonitsiory@gmail.com>
2025-05-01 20:23:42 +0200

    Will be deployed soon

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
-import ServerlessHttp from "serverless-http";
 
 dotenv.config();
 const app = express();
@@ -15,21 +14,19 @@ app.use(cors());
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-import userRouter from "../router/users.js";
-import categoryRouter from "../router/category.js";
-import expenseRouter from "../router/expense.js";
-import diverseQueries from "../router/diverseQueries.js";
+import userRouter from "./router/users.js";
+import categoryRouter from "./router/category.js";
+import expenseRouter from "./router/expense.js";
+import diverseQueries from "./router/diverseQueries.js";
 
 app.use("/user", userRouter);
 app.use("/category", categoryRouter);
 app.use("/expense", expenseRouter);
 app.use("/queries", diverseQueries);
-app.use("/api", Router)
 
 // * As default route, we have a the API's description in one HTML page
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "app.html"));
 });
 
-export const handler = ServerlessHttp(app)
-
+export default app;
